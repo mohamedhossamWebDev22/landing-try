dcfd0f221c485e40e98bbebac02e688ae073c40e
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2022-06-25 01:27:50 +0200

    maked the typing animation.

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 window.onscroll = function() {goUp()};
+window.onload = function (){typeAnim()}
 
-var btn_up = document.getElementById("up").style
+var btn_up = document.getElementById("up").style,
+txt = document.getElementById("txt");
+
+var i = 0,
+klam = "Redmi",
+speed = 300;
+
+function typeAnim(){
+    if (i < klam.length) {
+        txt.innerHTML += klam.charAt(i);
+        i++;
+
+        setTimeout(typeAnim, speed);
+    }
+}
 
 function goUp() {
     btn_up.visibility = "visible";
